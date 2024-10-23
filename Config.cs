1c06719239a7aabdd6b89a00efc199ccf6407cef
will <will@dandrade.net>
2024-10-23 15:03:06 -0400

    added commenting

--- Config.cs
+++ Config.cs
@@ -2,17 +2,17 @@
 public class Config
 {
 	
-    public static string PayloadPath { get; private set; } = @"C:\Windows\Test\Test2";
+    public static string PayloadPath { get; private set; } = @"C:\Windows\Test\Test2"; // This will be created if it doesnt already exist
     public static string PayloadName { get; private set; } = "TestPayload.exe";
 
 
 
-    public static string SecondaryWatchdogPath { get; private set; } = @"C:\Windows\Test\Test1";
+    public static string SecondaryWatchdogPath { get; private set; } = @"C:\Windows\Test\Test1"; // This will be created if it doesnt already exist
     public static string SecondaryWatchdogName { get; private set; } = "Windows Disk Maintenance.exe";
     public static string SecondaryWatchdogMutexName { get; private set; } = "SecondaryWDog";
 
 
-    public static string PrimaryWatchdogPath { get; private set; } = @"C:\Windows\Test";
+    public static string PrimaryWatchdogPath { get; private set; } = @"C:\Windows\Test"; // CRUCIAL: This is the foler you will be put both watchdogs and the payload into
     public static string PrimaryWatchdogName { get; private set; } = "Windows Service Manager.exe";
     public static string PrimaryWatchdogMutexName { get; private set; } = "PrimaryWDog";
     public static string PrimaryWatchdogFullPath { get; private set; } = Path.Combine(PrimaryWatchdogPath, PrimaryWatchdogName);
