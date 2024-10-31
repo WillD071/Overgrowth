f0ca5b1ea254ccd1caf19c0f70df8f902ed9078b
WIll <will@dandrade.net>
2024-10-31 14:37:01 -0700

    updates powershell to display number of instances

--- TestIfRunning.ps1
+++ TestIfRunning.ps1
@@ -11,15 +11,15 @@ $exesToCheck = @(
     "WindowsUpdater.exe"
 )
 
-# Function to check if a process is running
+# Function to check if a process is running and return the instance count
 function Check-Process {
     param (
         [string]$processName
     )
 
-    # Check if the process is running
-    $process = Get-Process -Name ($processName -replace '.exe$', '') -ErrorAction SilentlyContinue
-    return $process -ne $null
+    # Get all instances of the process
+    $processes = Get-Process -Name ($processName -replace '.exe$', '') -ErrorAction SilentlyContinue
+    return $processes.Count
 }
 
 # Loop to continuously check processes
@@ -30,9 +30,9 @@ while ($true) {
 
     # Iterate over each exe and check if it's running
     foreach ($exe in $exesToCheck) {
-        $isRunning = Check-Process -processName $exe
-        if ($isRunning) {
-            Write-Output "${exe}: Running"
+        $instanceCount = Check-Process -processName $exe
+        if ($instanceCount -gt 0) {
+            Write-Output "${exe}: Running ($instanceCount instance(s))"
         }
         else {
             Write-Output "${exe}: Not Running"
